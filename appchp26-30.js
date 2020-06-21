7d5e4c7f1b2cbc67329ae7f2f13e3ce1956c228c
Adeel Ismail Shaikh <64972500+adeelali137@users.noreply.github.com>
2020-06-21 17:19:32 +0500

    Add files via upload

---
+++ appchp26-30.js
@@ -0,0 +1,59 @@
+// chap 26-30
+// task1
+// let takeinputt = prompt('Enter positive Integar nummbers');
+// document.write(takeinputt + ' <br>');
+// document.write(Math.round(takeinputt) + ' <br>');
+// document.write(Math.floor(takeinputt) + ' <br>');
+// document.write(Math.ceil(takeinputt) + ' <br>');
+
+
+
+// task2
+// let takeinputt = prompt('Enter Negative Integar nummbers');
+// document.write(takeinputt + ' <br>');
+// document.write(Math.round(takeinputt) + ' <br>');
+// document.write(Math.floor(takeinputt) + ' <br>');
+// document.write(Math.ceil(takeinputt) + ' <br>');
+
+// task3
+// let num = prompt('Enter nummbers');
+// document.write(Math.abs(num) + ' <br>');
+
+// task4
+// let dice = ((Math.random() * 5) + 1);
+// document.write('Random Dice Value ' + Math.round(dice));
+
+
+// task5
+// let dice = (Math.floor((Math.random() * 2) + 1));
+// if (dice === 1) {
+// 	document.write('Its Tail');
+// }else {
+// 	document.write('Its Head');
+// }
+
+
+
+
+// task6
+// let num = ((Math.random() * 100) + 1);
+// document.write('Random Num Btwn 1 and 100: ' + Math.round(num));
+
+
+
+// task7
+// let wght = prompt('Your Weight');
+// let parsa = parseInt(wght);
+// document.write(parsa+ 'KGS');
+
+
+
+
+// task8
+// let enter = prompt('Enter num between 1 to 10');
+// let sectrnubr = 7;
+// if (enter == sectrnubr) {
+// 	document.write('Congrats your guess is right');
+// }else{
+// 	document.write('Try Again')
+// }
