9b9c9cbd7ffc6985a6504e9910f828ff8f3508d0
Adeel Ismail Shaikh <64972500+adeelali137@users.noreply.github.com>
2020-06-21 17:19:13 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,91 @@
+// chapter 21-25
+// task1
+// let fname = prompt('First Name');
+// let lname = prompt('Last Name');
+// alert('Good Morning ' + fname +" "+ lname);
+
+
+// task2
+// let mobilemodel = prompt('Enter tou fvt mobile');
+// alert('My favt mobile is '+ mobilemodel + " length od string is " + mobilemodel.length);
+
+/*task3*/
+// var sting = 'pakistan';
+// document.write(sting.indexOf('n'));
+
+// task4
+// var sting = 'Hello World';
+// document.write(sting.lastIndexOf('l'));
+
+// task5
+// var sting = 'pakistan';
+// document.write(sting.charAt(3));
+
+// // task6
+// let fname = prompt('First Name');
+// let lname = prompt(' Last Name');
+// let res = fname.concat(lname);
+// alert('Good Morning ' + res);
+
+// task7
+// let city = 'Hyderabad';
+// let change = city.replace('Hyder' , 'Islam');
+// document.write(change);
+
+// task8
+// var message = 'Ali and Sami are best friends. They play cricket and football together.';
+// var change = message.replaceAll('and' , '&')
+// document.write(change);
+
+// task9
+// var value = 472;
+// var name = value.toString();
+// console.log(typeof 'name');
+
+// task10
+// var name = 'peantunts';
+// document.write(name.toUpperCase())
+
+// task11
+// var name = 'javascript';
+// document.write(name.charAt(0).toUpperCase() + name.slice(1));
+
+// task12
+// var num = 35.44;
+// let final = num.toString();
+// document.write(final.slice(0,2) + 44);
+
+// task13
+// let name = prompt('Enteryour name');
+
+// if (name.includes('@')) {
+// 	alert('Enter valid USERname');
+// }else if (name.includes('.')) {
+// 	alert('Enter valid USERname');
+// }else if (name.includes(',')) {
+// 	alert('Enter valid USERname');
+// }else if (name.includes('!')) {
+// 	alert('Enter valid USERname');
+// }
+
+// task14
+// var name = prompt('Enter password');
+// var lowerCaseLetters = /[a-z]/g;
+// var upperCaseLetters = /[A-Z]/g;
+// var numbers = /[0-9]/g;
+// document.write(name.includes('lowerCaseLetters'))
+
+// task15
+// var university = 'University of Karachi.';
+// let arr = university.split('')
+// console.log(arr);
+
+// task16
+// let input = prompt('INPUR');
+// document.write(input.charAt(input.length - 1));
+
+
+// task17
+// let name = 'the quick brown fox jumps over the lazy dog';
+// document.write(name + ' <br>' + " in this the Occurnace of the is " + name.includes('the').length);
+
